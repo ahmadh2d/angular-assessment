12045a1b626b6793320f9074ca52960e9233eb3e
ahmadh2d <ahmadhas.b@gmail.com>
2025-06-04 18:09:23 +0000

    Add `prettier` plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = tseslint.config(
   {
@@ -11,9 +12,11 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      'prettier/prettier': 'error',
       "@angular-eslint/directive-selector": [
         "error",
         {
