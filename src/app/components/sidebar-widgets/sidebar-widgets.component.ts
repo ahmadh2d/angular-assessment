e1a8098929e6a6f53d135b1163be67c36bb67c54
ahmadh2d <ahmadhas.b@gmail.com>
2025-06-05 12:59:09 +0000

    Fix sidebar-widgets component UI

--- src/app/components/sidebar-widgets/sidebar-widgets.component.ts
+++ src/app/components/sidebar-widgets/sidebar-widgets.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-widgets',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './sidebar-widgets.component.html',
   styleUrl: './sidebar-widgets.component.css',
 })
-export class SidebarWidgetsComponent {}
+export class SidebarWidgetsComponent {
+  widgets = [
+    {
+      name: 'Sairaalalaheteet',
+      icon: 'folder',
+    },
+    {
+      name: 'Laboratoriotulaus',
+      icon: 'group',
+    },
+    {
+      name: 'Todistukset',
+      icon: 'description',
+    },
+    {
+      name: 'Reseptit',
+      icon: 'receipt',
+    },
+    {
+      name: 'Kuvantaminen',
+      icon: 'image',
+    },
+    {
+      name: 'Lääkelista',
+      icon: 'list',
+      href: '#',
+    },
+    {
+      name: '',
+      icon: 'add',
+    },
+  ];
+
+  widgetClick(widget: string) {
+    console.log(widget + ' clicked');
+  }
+}
