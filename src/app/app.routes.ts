e66483741b3068450e2db6fdb509000fdc62fb32
ahmadh2d <ahmadhas.b@gmail.com>
2025-06-05 07:05:16 +0000

    Add app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'patient-form',
+    // Lazy load PatientFormPageComponent
+    loadComponent: () =>
+      import('./pages/patient-form/patient-form.component').then(m => m.PatientFormComponent),
+    title: 'Patient Form',
+  },
+  {
+    path: 'dashboard',
+    // Lazy load DashboardPageComponent (assuming it's standalone)
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        // Adjust path as needed
+        m => m.DashboardComponent
+      ),
+    title: 'Dashboard',
+  },
+  // Other routes within this layout
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
