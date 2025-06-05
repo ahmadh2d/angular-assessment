f2f0d8b7650c3ac1a8676f1ee754e90435b69e3a
ahmadh2d <ahmadhas.b@gmail.com>
2025-06-05 12:55:16 +0000

    Fix quick-actions UI

--- src/app/components/quick-actions/quick-actions.component.ts
+++ src/app/components/quick-actions/quick-actions.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quick-actions',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './quick-actions.component.html',
   styleUrl: './quick-actions.component.css',
 })
-export class QuickActionsComponent {}
+export class QuickActionsComponent {
+  actions = [
+    {
+      name: 'Potilaan tarkastelu',
+      icon: 'description',
+    },
+    {
+      name: 'Potilaan vierit',
+      icon: 'tune',
+      badge: '1',
+    },
+    {
+      name: 'Lääkärin vierit',
+      icon: 'medical_services',
+    },
+    {
+      name: 'Työajat',
+      icon: 'schedule',
+    },
+    {
+      name: 'Kalenteri',
+      icon: 'calendar_today',
+    },
+    {
+      name: 'Asetukset',
+      icon: 'settings',
+    },
+    {
+      name: 'Terveysportti',
+      icon: 'local_hospital',
+    },
+  ];
+
+  actionClick(action: string) {
+    console.log(action + ' clicked');
+  }
+}
